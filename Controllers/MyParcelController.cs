9f2b7b1ebd3dbe88ce980ea7c93054f7abda6a6d
sheetal <sheetal.panchal10@gmail.com>
2025-05-18 20:14:26 +0200

    Initial commit

---
+++ Controllers/MyParcelController.cs
@@ -0,0 +1,54 @@
+
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MyParcelController : ControllerBase
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public MyParcelController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+    [HttpGet]
+    public IActionResult Test()
+    {
+        return Ok("MyParcel API is running ✅");
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateShipment([FromBody] ShipmentRequest input)
+    {
+        var shipment = new
+        {
+            data = new[]
+            {
+                new
+                {
+                    recipient = new
+                    {
+                        name = input.RecipientName,
+                        street = input.Street,
+                        postal_code = input.PostalCode,
+                        city = input.City,
+                        country_code = input.CountryCode,
+                        email = input.Email
+                    }
+                }
+            }
+        };
+
+        var client = _httpClientFactory.CreateClient();
+        client.DefaultRequestHeaders.Add("Authorization", "your-api-key-here");
+        client.DefaultRequestHeaders.Add("Content-Type", "application/json");
+
+        var response = await client.PostAsJsonAsync("https://api.myparcel.nl/shipments", shipment);
+        var content = await response.Content.ReadAsStringAsync();
+
+        return StatusCode((int)response.StatusCode, content);
+    }
+}
---
+++ Models/ShipmentRequest.cs
@@ -0,0 +1,10 @@
+
+public class ShipmentRequest
+{
+    public string RecipientName { get; set; }
+    public string Street { get; set; }
+    public string PostalCode { get; set; }
+    public string City { get; set; }
+    public string CountryCode { get; set; }
+    public string Email { get; set; }
+}
