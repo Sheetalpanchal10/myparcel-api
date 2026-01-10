88c516a9a8649d905dfb74534a4ee259a23920aa
Sheetalpanchal10 <124068998+Sheetalpanchal10@users.noreply.github.com>
2026-01-10 21:46:09 +0100

    Add dimensions to PhysicalProperties class

--- Models/ShipmentRequest.cs
+++ Models/ShipmentRequest.cs
@@ -41,6 +41,8 @@ public class InsuranceInfo
 }
 public class PhysicalProperties
 {
-    
+    public int Length { get; set; }
+    public int Width { get; set; }
+    public int Height { get; set; }
     public int Weight { get; set; }
 }
